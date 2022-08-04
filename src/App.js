a229e6998633c5960ce9a9a55ba322961c150167
Spuldz <mozgaedvards0@gmail.com>
2022-08-04 14:09:44 +0300

    Register page design completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import CustomizationPage from "./pages/CustomizationPage";
+import DefaultPage from "./pages/DefaultPage";
 import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<DefaultPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/customization" element={<CustomizationPage />} />
+      <Route path="/register" element={<RegisterPage />} />
     </Routes>
   );
 }
